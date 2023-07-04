7edccddf6489d55a3343ac70cbb7bd9e2566bcf7
ramzes-hk <ramzes.khakimov@ryerson.ca>
2023-07-03 21:55:31 -0400

    rewrote danger level parser

--- src/level_danger.py
+++ src/level_danger.py
@@ -6,22 +6,21 @@ url = "https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/en_U
 
 response = requests.get(url)
 levels = response.json().get("stages")
+activities_path = os.path.join(os.path.dirname(__file__), "..", "activities")
 
-for root, _, files in os.walk("activities", topdown=False):
-    path = f"activities/{os.path.relpath(root, url)}"
+for root, _, files in os.walk(activities_path, topdown=False):
     for file in files:
+        print(file)
         level_name = "_".join(os.path.splitext(file)[0].split("_")[1:])
         level = levels.get(level_name)
         if level:
             danger = level.get("dangerLevel")
             if danger:
                 dir = f"{root}/{file}"
+                item = {}
                 with open(dir, "r", encoding="utf-8") as f:
-                    try:
-                        item = json.load(f)
-                        item.update({"dangerLevel": danger})
-                    except json.decoder.JSONDecodeError:
-                        print(dir)
+                    item = json.load(f)
+                    item.update({"dangerLevel": danger})
                 with open(dir, "w", encoding="utf-8") as f:
                     json.dump(item, f, ensure_ascii=False, indent=4)
                     
