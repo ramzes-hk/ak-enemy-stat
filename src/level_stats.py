5a382a337e87383857c4c52f5924887ee90f6b8b
ramzes-hk <ramzes.khakimov@ryerson.ca>
2023-07-04 16:40:29 -0400

    rewrote average stats calculation

--- src/level_stats.py
+++ src/level_stats.py
@@ -1,23 +1,16 @@
 import json
 import os
 from collections import Counter
-import requests
 
-with open("level_dir.txt", "r", encoding="utf-8") as file:
-    path = file.read()
     
-with open("event_names.json", "r", encoding="utf-8") as file:
+with open(os.path.join(os.path.dirname(__file__), "..", "event_names.json"), "r", encoding="utf-8") as file:
     chapter_names = json.load(file)
-    
-with open("cn_enemies", "r", encoding="utf-8") as file:
-    enemy_db = json.load(file)
 
-url = "https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/en_US/gamedata/excel/stage_table.json"
 
-response = requests.get(url)
-stages = response.json().get("stages")
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "activities"))
+print(path)
 
-output = ""
+output = {}
 
 for root, _, files in os.walk(path, topdown=False):
     counter_maxHp = Counter()
@@ -25,80 +18,49 @@ for root, _, files in os.walk(path, topdown=False):
     counter_def = Counter()
     counter_magicResistance = Counter()
     counter_enemy = Counter()
-    
     dir_name = os.path.relpath(root, path)
     chapter_name = f"{chapter_names.get(dir_name, dir_name)}"
-    
-    output += f"\n{chapter_name}\n"
-    
+    print(chapter_name)
     for file in files:
-        level_name = "_".join(os.path.splitext(file)[0].split("_")[1:])
-        level = stages.get(level_name)
-        danger = level.get("dangerLevel")
-        file_path = f"{root}/{file}"
-        with open(file_path, "r", encoding="utf-8") as file:
-            data = json.load(file)
-        waves = data.get("waves")
-        enemy_db_refs = data.get("enemyDbRefs")
-        for wave in waves:
-            fragments = wave.get("fragments")
-            for fragment in fragments:
-                actions = fragment.get("actions")
-                for action in actions:
-                    if action.get("actionType") != 0:
-                        continue
-                    key = action.get("key")
-                    count = action.get("count", 0)
-                    counter_enemy.update({danger: count})
-                    for enemy in enemy_db_refs:
-                        if enemy.get("id") != key:
-                            continue
-                        overwritten = enemy.get("overwrittenData")
-                        default_enemy = enemy_db.get(key)
-                        maxHp = {
-                            danger: default_enemy.get("maxHp", 0) * count}
-                        atk = {
-                            danger: default_enemy.get("atk", 0) * count}
-                        def_ = {
-                            danger: default_enemy.get("def", 0) * count}
-                        magicResistance = {
-                            danger: default_enemy.get("magicResistance", 0) * count}
-                        if not overwritten:
-                            counter_maxHp.update(maxHp)
-                            counter_atk.update(atk)
-                            counter_def.update(def_)
-                            counter_magicResistance.update(magicResistance)
-                            continue
-                        attr = overwritten.get("attributes")
-                        if attr.get("maxHp").get("m_defined"):
-                            maxHp = {
-                                danger: attr.get("maxHp").get("m_value", 0) * count}
-                            counter_maxHp.update(maxHp)
-                        if attr.get("atk").get("m_defined"):
-                            atk = {
-                                danger: attr.get("atk").get("m_value", 0) * count}
-                            counter_atk.update(atk)
-                        if attr.get("def").get("m_defined"):
-                            def_ = {
-                                danger: attr.get("def").get("m_value", 0) * count}
-                            counter_def.update(def_)
-                        if attr.get("magicResistance").get("m_defined"):
-                            magicResistance = {
-                                danger: attr.get("magicResistance").get("m_value", 0) * count}
-                            counter_magicResistance.update(magicResistance)
-                        break
-                    break
-    for danger in counter_enemy.keys():
-        output += f"    {danger}\n"
-        output += f"        maxHP: {counter_maxHp.get(danger, 0)/counter_enemy.get(danger, 1):.1f}\n"        
-        output += f"        atk: {counter_atk.get(danger, 0)/counter_enemy.get(danger, 1):.1f}\n"
-        output += f"        def: {counter_def.get(danger, 0)/counter_enemy.get(danger, 1):.1f}\n"
-        output += f"        res: {counter_magicResistance.get(danger, 0)/counter_enemy.get(danger, 1):.1f}\n"
+        file_dir = os.path.join(root, file)
+        with open(file_dir, "r", encoding="utf-8") as f:
+            stage = json.load(f)
+        danger_level = stage.get("dangerLevel")
 
-with open("output1.txt", "w", encoding="utf-8") as f:
-    f.write(output)
+        for key in stage.keys():
+            if key == "dangerLevel":
+                continue
 
-         
-                    
-                        
+            enemy = stage.get(key)
+            count = enemy.get("count", 0)
+            maxHp = enemy.get("maxHp", 0) * count
+            atk = enemy.get("atk", 0) * count
+            def_ = enemy.get("def", 0) * count
+            magicResistance = enemy.get("magicResistance", 0) * count
+            
+            counter_enemy.update({danger_level: count})
+            counter_maxHp.update({danger_level: maxHp})
+            counter_atk.update({danger_level: atk})
+            counter_def.update({danger_level: def_})
+            counter_magicResistance.update({danger_level: magicResistance})
     
+    stats = {
+        "maxHP": counter_maxHp,
+        "atk": counter_atk,
+        "def": counter_def,
+        "res": counter_magicResistance
+    }
+    chapter = {} 
+    for key in counter_enemy.keys():
+        chapter.update({})
+        count = counter_enemy.get(key, 0)
+        danger = {} 
+        for stat, counter in stats.items():
+            danger.update({stat: "{:.1f}".format(counter.get(key, 0)/count)})
+            chapter.update({key: danger})
+    print(chapter)       
+    output.update({chapter_name: chapter})
+
+
+    with open("output.json", "w", encoding="utf-8") as f:
+        json.dump(output, f, indent=4, ensure_ascii=False)
